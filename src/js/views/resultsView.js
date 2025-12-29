127b1fd985b45b3b44f680ac4f89163336159ac3
idomaori98 <idoma@mta.ac.il>
2025-12-29 16:03:13 +0200

    factoring

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,5 +1,6 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
+import previewView from './previewView';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
@@ -7,34 +8,7 @@ class ResultsView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPriview).join('');
-  }
-
-  _generateMarkupPriview(result) {
-    const id = window.location.hash.slice(1);
-    const isActive = result.id === +id;
-    console.log(`Result ${result.id} - Hash: ${id} - Active: ${isActive}`);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${
-          result.id === +id ? 'preview__link--active' : ''
-        }" href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__year">${result.releaseDate.slice(0, 4)}</p>
-            <div class="preview__user-generated ${result.key ? '' : 'hidden'}">
-              <svg>
-                <use href="${icons}#icon-user"></use>
-              </svg>
-            </div>
-          </div>
-        </a>
-      </li>
-    `;
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
 
